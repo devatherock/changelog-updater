580b473f2fa0f4958729edb82e6d6130e579c5ca
devatherock <devatherock@gmail.com>
2022-05-26 08:45:55 -0500

    test: Added unit test for GithubService

--- src/main/java/io/github/devatherock/changelog/entities/PullRequestFile.java
+++ src/main/java/io/github/devatherock/changelog/entities/PullRequestFile.java
@@ -1,10 +1,15 @@
 package io.github.devatherock.changelog.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Class representing a file modified in a pull request
+ * 
+ * @author devaprasadh
+ *
+ */
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -12,7 +17,4 @@ public class PullRequestFile {
     private String status;
 
     private String filename;
-
-    @JsonProperty("contents_url")
-    private String contentsUrl;
 }
